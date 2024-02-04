3ee3b57bdcf1ee79bf1760add2b5281bfb8880ce
Владелец <intern-box@mail.ru>
2024-02-04 10:10:52 +0700

    Фиксация

--- Program.cs
+++ Program.cs
@@ -4,24 +4,7 @@
     {
         public static void Main()
         {
-            if (DateTime.Now.Month == 2)                            // Если февраль
-            {
-                if (DateTime.IsLeapYear(DateTime.Now.Year))         // и год високосный
-                {
-                    Console.WriteLine("февраль - високосный");
-                }
-                else
-                {
-                    Console.WriteLine("февраль - НЕ високосный");
-                }
-            }
-
-            switch (DateTime.Now.Day)
-            {
-                case 30: Console.WriteLine("тридцатое"); break;
-                
-                case 31: Console.WriteLine("тридцатьпервое"); break;
-            }
+            
         }
     }
 }
